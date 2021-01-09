5d9b62148e7a69b7c84556a35d695c0295a99c09
Андрей Митько <andrey@MacBook-Pro-5.local>
2021-01-09 12:31:27 +0300

    Initial Commit

---
+++ messageWow/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  messageWow
+//
+//  Created by Андрей Митько on 09/01/2021.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
